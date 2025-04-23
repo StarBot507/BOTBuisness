2f7a7fc4a3350b0e018bf9aaf2a43bee44bbcbd4
StarBot507 <starbot507@gmail.com>
2025-04-23 14:03:26 +0200

    Autre.py

--- cogs/Autre.py
+++ cogs/Autre.py
@@ -126,7 +126,7 @@ class AutreCog(commands.Cog):
         embed.add_field(name="", value=f"", inline=False)
         embed.add_field(name="💸 Perte possible", value=f"{perte:,}💰", inline=False)
 
-        await interaction.followup.send(embed=embed, ephemeral=True)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
     @discord.app_commands.command(name="simuler_vol", description="Simule un vol avec une probabilité d'échec et un gain estimé")
     async def simulateur_vol(self, interaction: discord.Interaction, cible_mention: str, initiator_mention: str):
@@ -239,7 +239,7 @@ class AutreCog(commands.Cog):
             embed.add_field(name="", value=f"", inline=False)
             embed.add_field(name="💸 Perte possible", value=f"{perte:,}💰", inline=False)  # Perte de 20% de ton cash
 
-            await interaction.followup.send(embed=embed, ephemeral=True)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
         else:
             # Si l'API ne répond pas correctement pour la cible
             await interaction.response.send_message("Erreur : Impossible de récupérer les données de la cible.")        
